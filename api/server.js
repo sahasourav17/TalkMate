f189cf15fa306833f3c3e543b66e993978685726
sahasourav17 <sahasourav1170@gmail.com>
2023-03-27 18:34:44 +0600

    add express server

---
+++ api/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { Configuration, OpenAIApi } = require("openai");
+
+dotenv.config({ path: "./.env" });
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.API_KEY,
+  })
+);
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  textInput = req.body.textInput;
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: textInput }],
+    });
+    res.send(response.data.choices[0].message.content);
+  } catch (err) {
+    res.send("Something Wrong. Please Try again.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
